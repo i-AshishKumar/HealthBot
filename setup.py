21f340c35412c1f195a59b7ab5fddc8193f2323b
Ashish Kumar Guntipalli <as589490@dal.ca>
2025-07-18 14:18:50 -0300

    first Commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'HealthBot',
+    version= '0.0.0',
+    author= 'Ashish Kumar Guntipalli',
+    author_email= 'ashishkumarg0877@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
